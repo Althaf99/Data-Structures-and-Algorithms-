9abadcfe43d57956928726c8b5736f299dfeef63
Althaf99 <mohamedalthaf872@gmail.com>
2022-01-18 08:58:09 +0530

    Added a number

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main {
         StringBuilderRev obj2 = new StringBuilderRev("Althaf");
 
         ThirdVariable obj3  = new ThirdVariable(10, 20);
-        System.out.println("Hello World");
+        System.out.println("Hello World3");
         CountWords objCountWords = new CountWords("AlthafffAAAAllll");
         CountWords2 objCountWords2 = new CountWords2("Althaf Azad is Name But People call me as Althaf But i prefer Mohammed Althaf Now i have made some changes by creating a new branch call Althaf underscore Azad");
     }
