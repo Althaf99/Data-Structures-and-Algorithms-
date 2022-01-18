f8aac83a7497a3da2b62e259bcf49033b59e7006
Althaf Azad <74148620+Althaf99@users.noreply.github.com>
2022-01-18 09:53:38 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main {
         CharArrayReverse obj1 = new CharArrayReverse("Althaf");
         System.out.println("-----------------------------------");
         StringBuilderRev obj2 = new StringBuilderRev("Althaf");
-
+        System.out.println("Hello World 1");
         ThirdVariable obj3  = new ThirdVariable(10, 20);
         CountWords objCountWords = new CountWords("AlthafffAAAAllll");
         CountWords2 objCountWords2 = new CountWords2("Althaf Azad is Name But People call me as Althaf But i prefer Mohammed Althaf Now i have made some changes by creating a new branch call Althaf underscore Azad");
